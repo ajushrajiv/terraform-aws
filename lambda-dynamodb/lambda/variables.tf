4189a2088086f233dd9760d5007886d3aeb3956d
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-07-31 14:27:11 +0200

    Update variables.tf

--- lambda-dynamodb/lambda/variables.tf
+++ lambda-dynamodb/lambda/variables.tf
@@ -3,7 +3,3 @@ variable "region" {
   default     = "eu-central-1"
 }
 
-#variable "filename" {
- # description = "Path to the zip file"
- # default     = "C:/Users/ajush/Desktop/version2-express-todo/todoexpress.zip.zip"
-#}
